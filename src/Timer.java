8db37ae98f8c5a4ea7e34ea755437bafb29f76ec
Chris Hellen <crimsonman7@gmail.com>
2023-11-05 16:44:59 -0600

    created report and timer classes

---
+++ src/Timer.java
@@ -0,0 +1,18 @@
+public class Timer {
+    int time;
+
+    public Timer() {
+    }
+
+    public static void runInteractiveMode(int time) {
+
+    }
+
+    public static void updateMemberRecords() {
+
+    }
+
+    public static void printAllReports () {
+
+    }
+}
