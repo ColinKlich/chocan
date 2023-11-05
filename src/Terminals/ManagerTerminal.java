5b3156a174b78976d4184f99fc28a95f2b869470
cdochow <cdochow@crimson.ua.edu>
2023-11-05 16:50:49 -0600

    updated

--- src/Terminals/ManagerTerminal.java
+++ src/Terminals/ManagerTerminal.java
@@ -1,28 +1,23 @@
-package src.Terminals;
-
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-import src.Operator.AccountsController;
-import src.Reports.EFTReport;
-import src.Reports.MemberReport;
-import src.Reports.ProviderReport;
-import src.Reports.SummaryReport;
-import src.Utilities.ServiceRecord;
-import java.text.ParseException;
-import java.util.Dictionary;
-import java.util.Objects;
-
 //Class for the Manager Terminal
 public class ManagerTerminal {
 
     //Tracks whether manager has been authenticated
     private boolean authenticated = false;
 
-
+    //Runs the manager terminal
     public int main(){
         
         boolean running = true;
 
+        while(running){
 
+            //Display/Prompt Login Credentials
+            System.out.println("Manager Terminal");
+            System.out.println("1. Login");
+            System.out.println("2. Quit");
+
+        }
+
+        return 0;
     }
 }
