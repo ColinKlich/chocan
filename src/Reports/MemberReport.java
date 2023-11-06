8e1a387ee70175ff39a3dfc75ca9d5d1f7b63fdc
Chris Hellen <crimsonman7@gmail.com>
2023-11-05 21:20:10 -0600

    updated report class

--- src/Reports/MemberReport.java
+++ src/Reports/MemberReport.java
@@ -3,15 +3,15 @@ package Reports;
 import java.util.*;
 
 public class MemberReport{
-    String memberName;
-    int memberNumber;
-    String memberStreetAddress;
-    String memberCity;
-    String memberState;
-    int memberZip;
-    List<Class<?>> servicesUsed = new ArrayList<>();
+    private String memberName;
+    private int memberNumber;
+    private String memberStreetAddress;
+    private String memberCity;
+    private String memberState;
+    private int memberZip;
+    private List<Class<?>> servicesUsed = new ArrayList<>();
 
     public static void print(){
-        
+
     }
 }
