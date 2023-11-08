12ac4be6a2aaa33a83368ad09dc6be602454f64b
Chris Hellen <crimsonman7@gmail.com>
2023-11-08 15:00:22 -0600

    updated services provided

--- src/services/ServiceProvided.java
+++ src/services/ServiceProvided.java
@@ -1,15 +1,20 @@
 package services;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class ServiceProvided {
-    private Date currentDateTime = now();
-    private Date serviceDate;
+    private String currentDateTime;
+    private String serviceDate;
     private int providerNumber;
     private int memberNumber;
     private int serviceCode;
     private String comments;
 
-    public ServiceProvided(Date serviceDate, int providerNum, int memberNum, int serviceCode, String comments){
-        this.serviceDate = serviceDate;
+    public ServiceProvided(int providerNum, int memberNum, int serviceCode, String comments){
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM-DD-YY HH:MM:SS");
+        this.serviceDate = date.format(format);
         this.providerNumber = providerNum;
         this.memberNumber = memberNum;
         this.serviceCode = serviceCode;
