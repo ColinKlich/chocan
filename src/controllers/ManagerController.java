8981d96bccdeffb1a95f1f4512cbbf2fd4c82fc8
Samuel Wisnoff <slwisnoff@crimson.ua.edu>
2023-11-08 14:55:23 -0600

    Added methods to ManagerController

--- src/controllers/ManagerController.java
+++ src/controllers/ManagerController.java
@@ -1,11 +1,47 @@
 package controllers;
 
 import Reports.MemberReport;
+import Terminals.ManagerTerminal;
 
 public class ManagerController {
-
-	public MemberReport getMemberReport() {
-		
+	//Goal: design a control system designed solely for Managers
+	//Objective #1: Need to have a member report retrieval function
+	/*
+	 * Need to have options so that you can pick what report you want
+	 * Needs to access the current state of the report as designed by the Weekly Accounting Procedure
+	 * 
+	 */
+	ManagerTerminal terminal;
+	
+	public ManagerController() {
+		terminal = new ManagerTerminal();
 	}
+	
+	public void getAllReports() {
+		
+		
+		return;
+	}
+	
+	public void getMemberReport() {
+		
+		return;
+	}
+	
+	public void getProviderReport() {
+		
+		
+		return;
+	}
+	
+	public void getSummaryReport() {
+		
+		
+		return;
+	}
+	
+	
+	
+
 
 }
