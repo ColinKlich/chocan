86b171425c04108b4e5d8f616fefbf7fcd376498
Chris Hellen <crimsonman7@gmail.com>
2023-11-08 14:29:40 -0600

    updated print function on summary report

--- src/Reports/SummaryReport.java
+++ src/Reports/SummaryReport.java
@@ -4,6 +4,8 @@ import accounts.Provider;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class SummaryReport {
@@ -11,8 +13,14 @@ public class SummaryReport {
     private int totalConsultations;
     private int overallFeeTotal;
     private String formattedReport;
+    private String currDate;
 
     public SummaryReport (List<Provider> providerList){
+
+        LocalDate date = LocalDate.now(); 
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+        currDate = date.format(format);
+
         this.totalProviders = providerList.size();
         for (Provider provider : providerList){
             this.totalConsultations += provider.getNumberOfConsultations();
@@ -35,21 +43,10 @@ public class SummaryReport {
     }
 
     public void print(){
+        File report = new File("SummaryReport" + currDate + ".txt");
 
         try {
-            File report = new File("SummaryReport.txt");
-            if (report.createNewFile()) {
-                System.out.println("File created: " + report.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        try {
-            FileWriter writer = new FileWriter("SummaryReport.txt");
+            FileWriter writer = new FileWriter(report.getName());
             writer.write(formattedReport);
             writer.close();
         } catch (IOException e) {
