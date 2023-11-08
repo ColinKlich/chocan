d0f28fdff2faad53cc9a865a550f1f5537cd3f84
cklich <cmklich@crimson.ua.edu>
2023-11-08 14:45:40 -0600

    Service provided

--- src/services/ServiceProvided.java
+++ src/services/ServiceProvided.java
@@ -1,5 +1,18 @@
 package services;
 
 public class ServiceProvided {
-    
+    private Date currentDateTime = now();
+    private Date serviceDate;
+    private int providerNumber;
+    private int memberNumber;
+    private int serviceCode;
+    private String comments;
+
+    public ServiceProvided(Date serviceDate, int providerNum, int memberNum, int serviceCode, String comments){
+        this.serviceDate = serviceDate;
+        this.providerNumber = providerNum;
+        this.memberNumber = memberNum;
+        this.serviceCode = serviceCode;
+        this.comments = comments;
+    }
 }
