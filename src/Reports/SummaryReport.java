ac07fb5aaa39caafebab4645d51ee80b04e2c52a
Samuel Wisnoff <slwisnoff@crimson.ua.edu>
2023-11-10 00:20:24 -0600

    Added second dummy data to Summary Report Get Function

--- src/Reports/SummaryReport.java
+++ src/Reports/SummaryReport.java
@@ -57,7 +57,37 @@ public class SummaryReport {
     
     public SummaryReport getSummaryReport(int reportNumber) {
     	SummaryReport report;
+    	//4 reports filled with sample info to test functionality
     	
+    	if (reportNumber == 1) {
+    		System.out.println("Summary Report 11/10/2023");
+    		System.out.println("Provider: Doug Sniffmeister. Number of Consultations: 35. Total Fee: $3456");
+    		System.out.println("Provider: Pig  Oinkfarmer. Number of Consultations: 25. Total Fee: $345");
+    		System.out.println("Provider: Bobby Wasabi. Number of Consultations: 10. Total Fee: $10000");
+    		System.out.println("Provider: Elon Musk. Number of Consultations: 1. Total Fee: $55");
+    		System.out.println("Total Number of Providers: 4");
+    		System.out.println("Total Number of Consultations: 71");
+    		System.out.println("Overall Total Fee: $13856");
+    		
+    	}
+    	else if (reportNumber == 2) {
+    		System.out.println("Summary Report 9/20/2023");
+    		System.out.println("Provider: Big Fish. Number of Consultations: 10. Total Fee: $50");
+    		System.out.println("Provider: Freddy Fazbear. Number of Consultations: 10. Total Fee: $300");
+    		System.out.println("Provider: Ben Shapiro. Number of Consultations: 5. Total Fee: $100");
+    		System.out.println("Provider: Doc McStuffins. Number of Consultations: 1. Total Fee: $50");
+    		System.out.println("Total Number of Providers: 4");
+    		System.out.println("Total Number of Consultations: 26");
+    		System.out.println("Overall Total Fee: $500");
+    		
+    	}
+    	else if (reportNumber == 3) {
+    		
+    	}
+    	else if (reportNumber == 4) {
+    		
+    		
+    	}
     	return  report;
     }
 }
