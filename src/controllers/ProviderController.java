2dc804455b2d50bfc386a9a873107bd435512992
kjmoran <kjmoran@crimson.ua.edu>
2023-11-10 21:54:35 -0600

    merge

--- src/controllers/ProviderController.java
+++ src/controllers/ProviderController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.Member;
+import java.util.Scanner;
 
 public class ProviderController {
 
@@ -14,30 +15,26 @@ public class ProviderController {
 
     }
 
+	/* MOVE to terminal */
     public void billChocAn(int amount, int serviceCode) {
-        /*
-         * validate member
-         */
-
-		InputStreamReader sr = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(sr);
-
-		System.out.println("Enter member number: ");
-
-		int memberNumber = 0;
-		try {
-			String fromUser = br.readLine();
-			memberNumber = Integer.parseInt(fromUser);
-		} catch (Exception e) {
-			memberNumber = DEFAULT_MEMBER_NUMBER;
-		}
-
 		/*
-		 * 
-		 */
+		  * validate member with card or member number
+		  enter date
+		  look up service code (enter service name and code is returned)
+		  key in service code
+		  display service name up to 20 chars or error message
+		  optionally enter comments
+		  */
+
+		Scanner scanner = new Scanner(System.in);
+		
 
 
-         System.out.println("Fee owed to provider: $" + totalFee);
+		  
+
+		
+
+
     }
 
 	public void requestProviderDirectory() {
