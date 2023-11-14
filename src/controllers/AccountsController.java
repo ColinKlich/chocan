9a8ffdb36dcad46c23f3f0e51202924cb73a4e71
Chris Hellen <crimsonman7@gmail.com>
2023-11-13 20:55:03 -0600

    updated accounts and manager controllers

--- src/controllers/AccountsController.java
+++ src/controllers/AccountsController.java
@@ -7,23 +7,21 @@ import java.text.ParseException;
 import java.util.Scanner;
 
 public class AccountsController {
-    private List<Member> members;
-    private List<Provider> providers;
+    Members members;
+    Providers providers;
     Scanner myObj = new Scanner(System.in);
 
     public AccountsController() {
-        Members members = new Members();
-        Providers providers = new Providers();
-        this.members = members.memberList;
-        this.providers = providers.providerList;
+        members = new Members();
+        providers = new Providers();
     }
 
     public List<Member> getMembers() {
-        return members;
+        return members.memberList;
     }
 
     public Member getMember(int memberNumber) {
-        for (Member member : members) {
+        for (Member member : members.memberList) {
             if (member.getNumber() == memberNumber) {
                 return member;
             }
@@ -32,7 +30,7 @@ public class AccountsController {
     }
 
     public Provider getProvider(int providerNumber) {
-        for (Provider provider : providers) {
+        for (Provider provider : providers.providerList) {
             if (provider.getNumber() == providerNumber) {
                 return provider;
             }
@@ -41,28 +39,28 @@ public class AccountsController {
     }
 
     public List<Provider> getProviders() {
-        return providers;
+        return providers.providerList;
     }
 
     public void addMember(String memberName, int memberNumber, String address, String city,
             String state, int zipCode) {
         Member member = new Member(memberName, memberNumber, address, city, state, zipCode);
-        this.members.add(member);
+        members.memberList.add(member);
     }
 
     public void addProvider(String providerName, int providerNumber, String address, String city,
             String state, int zipCode) {
         Provider provider =
                 new Provider(providerName, providerNumber, address, city, state, zipCode);
-        this.providers.add(provider);
+        providers.providerList.add(provider);
     }
 
     public void deleteMember(String name) {
-        members.removeIf(member -> member.getName().equals(name));
+        members.memberList.removeIf(memberList -> memberList.getName().equals(name));
     }
 
     public void deleteProvider(String name) {
-        providers.removeIf(provider -> provider.getName().equals(name));
+        providers.providerList.removeIf(providersList -> providersList.getName().equals(name));
     }
 
     public void createMember() {
@@ -79,7 +77,7 @@ public class AccountsController {
         System.out.println("Member Zip Code: ");
         int zipCode = Integer.valueOf(myObj.nextLine());
         Member member = new Member(name, number, address, city, state, zipCode);
-        this.members.add(member);
+        members.memberList.add(member);
         System.out.println("Member Added!" + "\n");
     }
 
@@ -102,7 +100,7 @@ public class AccountsController {
         System.out.println("Provider Zip Code: ");
         int zipCode = Integer.valueOf(myObj.nextLine());
         Provider provider = new Provider(name, number, address, city, state, zipCode);
-        this.providers.add(provider);
+        providers.providerList.add(provider);
         System.out.println("Provider Added!" + "\n");
     }
 
