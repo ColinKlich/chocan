5859cf65a8b5fdde5ec1ea1bfaa461d3504153d8
cdochow <cdochow@crimson.ua.edu>
2023-11-14 17:25:32 -0600

    finally finished Junit, fml. I hate user input

--- src/JUnit/CalebDochowTest.java
+++ src/JUnit/CalebDochowTest.java
@@ -2,60 +2,35 @@
 package JUnit;
 
 import static org.junit.Assert.*;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import services.*;
 import accounts.*;
-import controllers.AccountsController;
-import terminals.*;
-import utilities.Providers;
-import java.util.List;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CalebDochowTest {
-
-    Manager admin;
-    ManagerTerminal managerTerminal;
-    AccountsController accounts;
     Provider provider;
-    ProviderTerminal providerTerminal;
     Member member;
-    Providers providers;
-	List<Service> services;
+    
 
     @Before
     public void setUp() throws Exception {
-		admin = new Manager("admin", "admin", "testing123");
-        accounts = new AccountsController();
-		member = new Member("John Doe",123456789,"123 1st Street","Tuscaloosa","AL",12345);
-
-       // managerTerminal = new ManagerTerminal(accounts);
+		provider = new Provider("Jane Doe", 123123123, "123 2nd Street", "Tuscaloosa", "AL", 35405);
+		member = new Member("John Doe",123456789,"123 1st Street","Tuscaloosa","AL",35405);
 
     }
 
     @Test
-    public void verifyManagerTest() {
-        // Set up System.in to provide the expected inputs
-        String input = "admin\n" + "testing123\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-
-        // Use setInputSource to provide the custom input source
-        managerTerminal.setInputSource(in);
-
-        assertTrue(managerTerminal.verifyManager(admin));
+    public void getCity() {
+        assertEquals("Tuscaloosa", provider.getCity());
     }
 
     @Test
-    public void verifyProviderTest() {
-        assertTrue(providerTerminal.verifyProvider(accounts));
+    public void getState() {
+        assertEquals("AL", provider.getState());
     }
 
-    @Test
+    @Test //Hai's
     public void getZipCodeTest() {
-        assertEquals(12345, member.getZipCode());
+        assertEquals(35405, member.getZipCode());
     }
 
 
