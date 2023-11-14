a155e822fcf7a10ff936877892ab0513b7f26c83
Chris Hellen <crimsonman7@gmail.com>
2023-11-13 19:34:35 -0600

    added providers and members class

--- src/utilities/Members.java
+++ src/utilities/Members.java
@@ -1,5 +1,23 @@
 package utilities;
 
+import java.util.*;
+import accounts.Member;
+import services.ServiceProvided;
+
 public class Members {
-    
+    public List<Member> memberList;
+    public Members(){
+        Member mem1 = new Member("Hai Hoang", 12235, "1234 Meadow Ln", "Tuscaloosa", "AL", 12345);
+        Member mem2 = new Member("Colin Klich", 12114, "1234 Meadow Ln", "Tuscaloosa", "AL", 12345);
+        Member mem3 = new Member("Sam Wisnoff", 12345, "1234 Meadow Ln", "Tuscaloosa", "AL", 12345);
+        ServiceProvided service1 = new ServiceProvided("Consultation", "02-19-2003", 12345, "Chris Hellen", 66979, 12345, "Hai Hoang", 12, null);
+        ServiceProvided service2 = new ServiceProvided("Excercise", "02-19-2004", 12346, "Caleb Dochow", 12158, 12345, "Hai Hoang", 12, null);
+        ServiceProvided service3 = new ServiceProvided("Therapy", "02-19-2005", 12347, "Kara Moran", 60246, 12345, "Hai Hoang", 12, null);
+        mem1.addServiceUsed(service1);
+        mem2.addServiceUsed(service2);
+        mem3.addServiceUsed(service3);
+        memberList.add(mem1);
+        memberList.add(mem2);
+        memberList.add(mem3);
+    }
 }
