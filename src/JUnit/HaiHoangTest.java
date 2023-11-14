b835da23a86414fe06df48cb5e6381016726bbf2
Hai Hoang <hlhoang@crimson.ua.edu>
2023-11-13 18:40:29 -0600

    no message

---
+++ src/JUnit/HaiHoangTest.java
@@ -0,0 +1,31 @@
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import accounts.Member;
+
+public class HaiHoangTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMemberCreate() {
+		Member x = new Member();
+	}
+
+	@Test
+	public void testSetMemberInformation() {
+		
+	}
+
+	@Test
+	public void testProviderReportCreate() {
+		
+	}
+
+}
